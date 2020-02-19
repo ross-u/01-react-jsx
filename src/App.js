650a059504b6d9e9d62490bb3632b1ed23fa5241
ross-u <cirkovic.uros.srb@gmail.com>
2020-02-19 10:22:33 +0100

    Update App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from "react";
 import "./App.css"; // with React CSS files are imported directly into the .js file
 
-const element = React.createElement;
-
-// `createElement` syntax :
-//	React.createElement(component, props, ...children)
-
-
 class App extends React.Component {
   render() {
-    return element('div', { className: 'App' }, [
-      element('h1', null, 'Hello Ironhackers'),
-      element('h3', null, 'React without JSX')
-    ]);
+    return (
+      <div className="App">
+        <h1> Hello Ironhackers! </h1>
+        <h3>React Intro </h3>
+      </div>
+    );
   }
-
-
+}
 
 export default App;
